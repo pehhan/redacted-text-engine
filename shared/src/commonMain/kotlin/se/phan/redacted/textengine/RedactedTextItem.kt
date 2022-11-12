e360b54bf0b34aa0585937856ef769a1bc70c32b
Pehr Hansson <pehr.hansson@parkster.se>
2022-11-12 16:46:52 +0100

    chore: Formatted code.

--- shared/src/commonMain/kotlin/se/phan/redacted/textengine/RedactedTextItem.kt
+++ shared/src/commonMain/kotlin/se/phan/redacted/textengine/RedactedTextItem.kt
@@ -3,9 +3,6 @@ package se.phan.redacted.textengine
 sealed class RedactedTextItem
 
 data class Word(val str: String) : RedactedTextItem()
-
 data class Punctuation(val sign: Char) : RedactedTextItem()
-
 object Space : RedactedTextItem()
-
 object Newline : RedactedTextItem()
